6899599d8c757f587b96db1968779688e57a57e7
ZhangYang <1944952458@qq.com>
2017-05-14 18:29:35 +0800

    config sames error!page show nothing

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module'
+    },
+    env: {
+        browser: true,
+    },
+    // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+    extends: 'standard',
+    // required to lint *.vue files
+    plugins: [
+        'html'
+    ],
+    // add your custom rules here
+    'rules': {
+        'eol-last': ["error", "always"],
+        'no-undef': ["error", {"typeof": true}],
+        //use IDE's default indent
+        'indent': 0,
+        // allow paren-less arrow functions
+        'arrow-parens': 0,
+        //allow semicolon
+        'semi': ['error', 'always'],
+        // allow async-await
+        'generator-star-spacing': 0,
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    }
+};
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <vheader></vheader>
+        <div class="tab">
+            I am tab
+        </div>
+        <div class="content">
+            I am content
+        </div>
+    </div>
+</template>
+<script>
+    import header from './components/header/header.vue';
+    export default {
+        components: {
+            'vheader': header
+        }
+    };
+</script>
+<style>
+</style>
